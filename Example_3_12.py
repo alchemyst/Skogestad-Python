91327286ea5e0584b47e7433959014d44309f335
unknown <Robbie@Robbie-PC.(none)>
2012-04-01 17:35:53 +0200

    Examples 3.11-12

---
+++ Example_3_12.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# The following code performs Example 3.12 of Skogestad. 
+# Here the RGA, iterative RGA, condition number and minimized
+# condition number is calculated for constant transfer function G. 
+# The minimized coondition number is not implemented. 
+# Examples 3.13-15 are all similar
+
+def RGA(A):
+    A = np.multiply(A, np.transpose(np.linalg.pinv(A)))
+    return A
+
+def condnum(A):
+    gamma = A[0]/A[-1]
+    return gamma
+
+def IterRGA(A, n):
+    for i in range(1, n):
+            A = RGA(A)
+    return A
+
+def RGAnumber(A):
+    RGAnum = np.sum(np.abs(RGA(A) - np.identity(len(A))))
+    return RGAnum
+    
+            
+G = np.matrix([[100, 0], [0, 1]])
+
+[U, S, V] = np.linalg.svd(G)
+
+R = RGA(G)
+ItR = IterRGA(G, 4)
+numR = RGAnumber(G)
+numC = condnum(S)
+
+print 'RGA:\n', R, '\nIterative RGA:\n', ItR, '\nCondition Number:\n', numC
+
