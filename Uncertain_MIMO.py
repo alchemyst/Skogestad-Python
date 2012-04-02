bc3a35d595d0ddf5ac9212ea572b0fc12e512860
Eben <eben@zeil.co.za>
2012-04-02 11:22:19 +0200

    Updated repo and finished chapter 7 analysis
    PEAK_MIMO.py is a program to do all the analysis except dead times
    analysis on T and S peak values have not been added yet.
    
    Starting with Uncertainty's programming.

--- Uncertain_MIMO.py
+++ Uncertain_MIMO.py
@@ -1,14 +1,31 @@
-import numpy as np
+import numpy as np 
 import scipy.linalg as sc_lin
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
 
-# start deriving and implementing equation for uncertainty
+# start dirriving and implementing equation for uncertainty 
 
 
 def uncertian_MIMO():
-
+    
     #upper bounds on S'
     #eq6-88 pg 248
-
-    #check input and output condition number along with RGA elements
+    
+    #check input and output condision number along with RGA elements 
+    
+    
+    #nuquist plot with perturbed plant models generating all possible plants nuquist plots  
+    #pg 268-271 skogestad 
+    #using method outlined in 7.4.3
+    
+    #use mesh function from CSC research project to subpliment usample function of matlab
+    
+    #################
+    #most probably going to use State Space , easier to discribe uncertainty and is numerically easier
+    #################
+    
+    
+    #### 
+    #eq 7-36 and 7-37 
+    #higher freqeuncy uncertainty
+    ####
