57d8885020054a3c8f8a9a48d56edf434979bc7d
Eben <eben@zeil.co.za>
2012-04-03 21:01:39 +0200

    Added PEP8 to PEAK_MIMO and started perturbations
    Perturbations.py program could be copied to make any amount of
    perturbations of a plant given the maximum and minimum values of a
    certain variable.
    
    Uncertianty.py is the start of the uncertainty analysis, thus far only
    the equations that is going to be programmed is written down, the
    actual equations are going to follow soon.
    
    PEAK_MIMO.py could be used for controllability of any MIMO system. PEP8
    is applied to this program.

--- Uncertain_MIMO.py
+++ Uncertain_MIMO.py
@@ -1,31 +1,49 @@
-import numpy as np 
+import numpy as np
 import scipy.linalg as sc_lin
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 
-# start dirriving and implementing equation for uncertainty 
+# start deriving and implementing equation for uncertainty this is for SISO systems 
 
 
-def uncertian_MIMO():
-    
+def G(s):
+    """returns value of the nominal plant"""
+    G= 
+    return G 
+
+
+def Gp(s,vec,i):
+    """vec contains all perturbations of the varaibles to be used
+    i is the counter to be used
+    Gp is the perturbed plant"""
+
+    Gp= 
+    return Gp
+
+def Bound_SISO_wi():
+
     #upper bounds on S'
     #eq6-88 pg 248
-    
-    #check input and output condision number along with RGA elements 
-    
-    
-    #nuquist plot with perturbed plant models generating all possible plants nuquist plots  
-    #pg 268-271 skogestad 
+
+    #check input and output condition number along with RGA elements
+
+
+    #nuquist plot with perturbed plant models generating all possible plants nuquist plots
+    #pg 268-271 skogestad
     #using method outlined in 7.4.3
-    
+
     #use mesh function from CSC research project to subpliment usample function of matlab
-    
+
     #################
     #most probably going to use State Space , easier to discribe uncertainty and is numerically easier
     #################
-    
-    
-    #### 
-    #eq 7-36 and 7-37 
-    #higher freqeuncy uncertainty
+
+    ####
+    #eq 7-36 and 7-37
+    #higher frequency uncertainty
+    ####
+
+    ####
+    #calculating w_i for spesific plant with a set amount of perturbations
+    #multiplicative uncertainty
     ####
