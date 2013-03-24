13d6e09eed2e71e4d71b2cccf095bd27078eb9bb
Carl Sandrock <carl.sandrock@up.ac.za>
2013-03-24 11:26:35 +0200

    Applied PEP8

--- Example_4_4.py
+++ Example_4_4.py
@@ -19,19 +19,19 @@ G = sign.lti(A, B, C, D)
 
 # Question: Why does A.transpose give the same eigenvectors as the book and not plain A??
 # Answer: The eig function in the numpy library only calculates the right hand eigenvectors. The Scipy library provides a eig function in which you can specify whether you want left
-# or right handed eigenvectors. To determine controllability you need to calculate the input pole vectors which is dependant on the left eigenvectors. 
+# or right handed eigenvectors. To determine controllability you need to calculate the input pole vectors which is dependant on the left eigenvectors.
 # Calculate eigen vectors and pole vectors
 val, vec = LA.eig(A, None, 1, 0, 0, 0)
-U1 = np.dot(B.T,vec[:,0].T)
-U2 = np.dot(B.T,vec[:,1].T)
+U1 = np.dot(B.T, vec[:, 0].T)
+U2 = np.dot(B.T, vec[:, 1].T)
 
 # Set-up controlability Matrix
 Con = np.zeros([2, 2])
 Con[:, 0] = B.T
-Con[:, 1] = (A*B).T
+Con[:, 1] = (A * B).T
 n = lin.matrix_rank(Con)
 
-P = LA.solve_lyapunov(A,-B*B.T)
+P = LA.solve_lyapunov(A, -B * B.T)
 
 # Display results
 print '\nThe Transfer function realization is:'
