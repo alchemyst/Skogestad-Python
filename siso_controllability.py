baef150d4887235e77ed260740b8a503a15da86e
St Elmo <stelmozors@gmail.com>
2013-03-24 17:38:15 +0200

    Finished the important SISO controllability rules.
    This module should make analysis of a SISO system easier as
    it draws all the pertinent graphs for you.

--- siso_controllability.py
+++ siso_controllability.py
@@ -27,7 +27,7 @@ Rule 2: Speed of response to track reference changes.
 Similar to rule 1 but this time you want |S| < 1\R where
 R is the max allowed reference change relative to the allowed error.
 """
-def rule_one(S, Gd, R,  freq = np.arange(0.001, 1,0.001)):
+def rule_one_two(S, Gd, R = 1.1,  freq = np.arange(0.001, 1,0.001)):
     """
     Parameters: S => sensitivity transfer function
                 Gd => transfer function of the disturbance
@@ -40,8 +40,8 @@ def rule_one(S, Gd, R,  freq = np.arange(0.001, 1,0.001)):
     mag_i, phase_i, freq_i = cn.bode_plot(inv_gd, omega = freq)
     plt.clf()
     unity = [1]*len(freq)
-    inv_R = [1/R]*len(freq)
-    
+    inv_R = [1.0/R]*len(freq)
+
     plt.loglog(freq_s, inv_R, color = "green", lw = 3.0, ls = "--", label = "1/R")
     plt.loglog(freq_s, unity, color ="black", ls = ':')
     plt.loglog(freq_s, mag_s,  color = "blue", label = "|S|")
@@ -53,20 +53,103 @@ def rule_one(S, Gd, R,  freq = np.arange(0.001, 1,0.001)):
 
 """
 Rule 3 and 4: |u| < 1 to ensure your input is in the allowed range
+              given that |e| < 1
 
 Rule 3: Input constraints arising from disturbances.
+|G| > |Gd| - 1 for acceptable control where |Gd| > 1
+ 
+Rule 4: Input constraints arising from set points.
+|G| > R - 1 for acceptable control
+"""
+def rule_three_four(G, Gd, R = 1.1, perfect = True, freq = np.arange(0.001, 1,0.001)):
+    """
+    Parameters: G => transfer function of system
+                Gd => transfer function of disturbances on system
+                R => max allowed reference change relative to e (R > 1)
+                perfect => Boolean: True then assumes perfect control
+    """
+    mag_g, phase_g, freq_g = cn.bode_plot(G, omega = freq)
+    plt.clf()
+    mag_gd, phase_gd, freq_gd = cn.bode_plot(Gd, omega = freq)
+    plt.clf()
+    
+    plt.subplot(211)
+    
+    if perfect:
+        plt.loglog(freq_gd, mag_g, color = "red", label = "|G|", ls = "--")
+        plt.loglog(freq_gd, mag_gd, color = "blue", label = "|Gd|")
+    else:
+        gd_min = mag_gd - 1
+        gd_min = [x for x in gd_min if x > 0]
+        freq_all = [freq_gd[x] for x in range(len(freq_gd)) if (mag_gd[x]-1) > 0]
+        mag_g_all = [mag_g[x] for x in range(len(freq_gd)) if (mag_gd[x]-1) > 0]
+        plt.loglog(freq_all, gd_min, color = "blue", label = "|Gd| - 1")
+        plt.loglog(freq_all, mag_g_all, color = "red", label = "|G|", ls = "--")
+     
+    plt.legend(loc = 3)
+    plt.grid()
+    plt.ylabel("Magnitude")
+    plt.xlabel("Frequency [rad/s]")
+    
+    plt.subplot(212)
+    plt.loglog(freq_g, mag_g, color = "red", label = "|G|", ls = "--")
+    if perfect:
+        R_stretch = len(freq_g)*[np.abs(R)]
+        plt.loglog(freq_g, R_stretch, color = "blue", label = "|R|")
+    else:
+        R_stretch = np.subtract(len(freq_g)*[np.abs(R)], [1]*len(freq_g) )
+        plt.loglog(freq_g, R_stretch, color = "blue", label = "|R|-1")
+    plt.legend(loc = 1)
+    plt.grid()
+    plt.ylabel("Magnitude")
+    plt.xlabel("Frequency [rad/s]")
+    
+    
+    
 
-Rule 4: Input constraints arising from setpoints.
+"""
+Rules 5 - 8:
+These are more like bounds on the bandwidth given certain characteristics of the
+system.
+They do depend on the bound of either S or T... (S for zeros, T for poles via internal stability)
+The important thing is really just Theorem 5.3 + the interpolation constraints... everything else
+follows from that.
 """
 
 
+"""
+This method will just show the intersection of the dead time frequency
+with the loop gain magnitude. (forms part of rule 1 and rule 5)
+"""
+def dead_time_bound(L, Gd, deadtime, freq = np.arange(0.001, 1,0.001)):
+    """
+    Parameters: L => the loop transfer function
+                Gd => Disturbance transfer function
+                deadtime => the deadtime in seconds of the system
+    """
+    mag, phase, omega = cn.bode_plot(L, omega = freq)
+    mag_d, phase, omega = cn.bode_plot(Gd, omega = freq) 
+    plt.clf()
+    
+    gm, pm, wg, wp_L = cn.margin(L)
+    gm, pm, wg, wp_Gd = cn.margin(Gd)
+    
+    freq_lim = [freq[x] for x in range(len(freq)) if mag[x] > 0.1 and mag[x] < 10]
+    mag_lim = [mag[x] for x in range(len(freq)) if mag[x] > 0.1 and mag[x] < 10]
+    
+    plt.loglog(freq_lim, mag_lim, color = "blue", label = "|L|")
+    
+    dead_w = 1.0/deadtime
+    ymin, ymax = plt.ylim()
+    plt.loglog([dead_w, dead_w], [ymin, ymax], color = "red", ls = "--", label = "dead time frequency")
+    plt.loglog([wp_L, wp_L], [ymin, ymax], color = "green", ls =":", label = "w_c")
+    plt.loglog([wp_Gd, wp_Gd], [ymin, ymax], color = "black", ls = "--", label = " w_d")
+    print ("You require feedback for disturbance rejection up to (w_d) = " + str(wp_Gd) + 
+           "\n Remember than w_B < w_c < W_BT and  w_d < w_B hence w_d < w_c.")
+    print "The upper bound on w_c based on the dead time (wc < w_dead = 1/dead_seconds) = " + str(1.0/deadtime)
+    
+    plt.legend(loc = 3)
+    
 
-"""Rule 5:  Time delay."""
-
-"""Rule 6: Tight control at low frequencies with an RHP zero."""
-
-"""Rule 7: Not part of the syllabus..."""
-
-"""Rule 8: Real open loop unstable pole."""
 
 
