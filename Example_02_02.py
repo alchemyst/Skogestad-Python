a238f9874bdf0662e2f28ab116c408b7715eb12e
Schalk van Heerden <vhschalk@gmail.com>
2013-04-25 08:22:41 +0200

    Update plots and legends in Ch 2

---
+++ Example_02_02.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.signal as scs
+from utils import feedback, tf
+
+# Process model of G with various Controller Gains
+s = tf([1, 0], 1)
+G = 3*(-2*s + 1) / ((10*s + 1)*(5*s + 1))
+
+tspan = np.linspace(0, 50, 100)
+
+#  calculating the time domian response
+for K in [0.5, 1.5, 2, 2.5]:
+    T = feedback(G * K, 1)
+    [t, y] = T.step(0, tspan)
+    plt.plot(t, y)
+
+plt.legend(["Kc = 0.5","Kc = 1","Kc = 2","Kc = 2.5"])    
+plt.xlabel('Time [s]')
+plt.show()
