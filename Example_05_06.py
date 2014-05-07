e3d9488cf4af5d7e8afdf7a233bfda5021b41156
Schalk van Heerden <vhschalk@gmail.com>
2014-05-07 15:26:05 +0200

    Change visual appearance

--- Example_05_06.py
+++ Example_05_06.py
@@ -1,35 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.signal as scs
 from utils import tf, feedback
 
-# Process model of G with various Controller Gains
+# Process model transfer function
 s = tf([1, 0])
-G = (-s+1)/(s+1)
+G = (-s + 1)/(s + 1)
 
-# Controllers
-Ks = [Kc*((s+1)/s)*(1/(0.05*s+1)) for Kc in [0.2, 0.5, 0.8]]
+# Controllers transfer function with various controller gains
+Ks = [Kc * ((s + 1)/s) * (1 / (0.05 * s + 1)) for Kc in [0.2, 0.5, 0.8]]
 
-# Closed loop transfer functions
-Ts = [feedback(G*K, 1) for K in Ks]
+# Complementary sensitivity transfer functions
+Ts = [feedback(G * K, 1) for K in Ks]
 
-#  The time domian response
-plt.subplot(2, 1, 1)
+plt.figure('Figure 5.7')
+w = np.logspace(-2, 2, 1000)
+wi = w * 1j
+plt.subplot(1, 2, 1)
+plt.title('(a) Sensitivity function')
+for T in Ts:
+    S = 1 - T
+    plt.loglog(w, abs(S(wi)))
+plt.xlabel('Frequency [rad/s]')
+plt.ylabel('Magnitude |S|')
+plt.legend(["Kc=0.2", "Kc=0.5", "Kc=0.8"],
+           bbox_to_anchor=(0, 1.03, 1, 0), loc=3, ncol=3)
+           
+plt.subplot(1, 2, 2)
+plt.title('(b) Response to step in reference')
 tspan = np.linspace(0, 5, 100)
 for T in Ts:
     [t, y] = T.step(0, tspan)
     plt.plot(t, y)
-plt.xlabel('Time (sec)')
+plt.xlabel('Time [sec]')
 plt.ylabel('y(t)')
 
-# sensitivity function
-w = np.logspace(-2, 2, 1000)
-wi = w*1j
-plt.subplot(2, 1, 2)
-for T in Ts:
-    S = 1 - T
-    plt.loglog(w, abs(S(wi)))
-
-plt.xlabel('Frequency (rad/s)')
-plt.ylabel('Magnitude (S)')
 plt.show()
