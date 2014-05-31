62f7f986228d3dcb5eefbaee6578938906005241
Carl Sandrock <carl.sandrock@up.ac.za>
2014-05-31 17:52:51 +0200

    Added support for numpydocs in documentation

--- conf.py
+++ conf.py
@@ -13,6 +13,26 @@
 
 import sys, os
 
+# Mock the utilities used by the code
+# see http://blog.rtwilson.com/how-to-make-your-sphinx-documentation-compile-with-readthedocs-when-youre-using-numpy-and-scipy/
+
+import mock
+ 
+MOCK_MODULES = ['numpy',
+                'numpy.linalg',
+                'numpy.matlib',
+                'scipy',
+                'scipy.linalg',
+                'scipy.optimize',
+                'scipy.optimize.fmin',
+                'scipy.signal', 
+                'matplotlib', 
+                'matplotlib.pyplot', 
+                'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -25,7 +45,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode', 'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
