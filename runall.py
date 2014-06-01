b45ae4075c245d8786c6de2e80d2f7f3d656d881
Carl Sandrock <carl.sandrock@up.ac.za>
2014-06-01 16:29:37 +0200

    Build table of results in runall.py

--- runall.py
+++ runall.py
@@ -3,11 +3,15 @@
 import sys
 import glob
 from matplotlib import pyplot as plt
+import traceback
+from operator import itemgetter
 
 # disable show in figures
 plt.show = lambda: False
 
 types = ['Figure', 'Example', 'Exercise']
+
+statustable = []
 
 if __name__ == "__main__":
     for t in types:
@@ -15,4 +19,13 @@ if __name__ == "__main__":
         print "Running", len(files), t + "s"
         for f in files:
             print " ", f
-            execfile(f)
+            try:
+                execfile(f)
+                statustable.append([f, 'Success'])
+            except Exception, err:
+                print traceback.format_exc()
+                statustable.append([f, 'Failed'])
+
+    statustable.sort(key=itemgetter(1))
+    for filename, status in statustable:
+        print filename, status
