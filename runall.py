317a26ac5f1facbbc3efb4c33b47ee39ecca27cd
Carl Sandrock <carl.sandrock@up.ac.za>
2015-03-08 11:39:13 +0200

    Fixed runner by inlining function
    execfile doesn't work right with global names if called from a function

--- runall.py
+++ runall.py
@@ -5,10 +5,13 @@ import os
 import traceback
 from operator import itemgetter
 import re
+from collections import Counter
 
 # disable show in figures
 import matplotlib.pyplot as plt
 plt.show = lambda: None
+
+statuscounter = Counter()
 
 itemparser = re.compile('(?P<kind>.*) (?P<chapter>.*)\.(?P<number>.*)')
 allitems = open('allitems.txt').read().splitlines()
@@ -17,20 +20,9 @@ kinds = ['Figure', 'Example', 'Exercise']
 
 statustable = []
 
-
-def runfile(f):
-    try:
-        execfile(f)
-        return 'Success', None
-    except Exception, err:
-        return 'Failed', traceback.format_exc()
-
-
 if __name__ == "__main__":
     for item in allitems:
         kind, chapter_c, number_c = itemparser.match(item).groups()
-
-        status = 'Not implemented'
 
         number = int(number_c)
 
@@ -43,8 +35,19 @@ if __name__ == "__main__":
 
         filename = mask.format(kind, chapter, number)
         if os.path.exists(filename):
-            status, message = runfile(filename)
+            try:
+                execfile(filename)
+                status= 'Success'
+            except Exception, err:
+                status = 'Failed'
+                message = traceback.format_exc()
+        else:
+            status = 'Not implemeted'
+
+        statuscounter[status] += 1
 
         print kind, chapter, number, status
         if status == 'Failed':
             print message
+
+    print statuscounter
