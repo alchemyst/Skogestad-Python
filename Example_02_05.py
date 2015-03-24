c18b588663a8e0dac42b8c4464f2e24f182ddac6
AliciaM1 <leroux.alicia@yahoo.com>
2015-03-24 21:11:26 +0200

    Fixed u(t) plot and improved display of variables
    Fixed u(t) plot: u=S*K and improved the display of the GM, PM, wc, wb,
    and wbt variables.
    PM conversion from degrees to radians corrected

--- Example_02_05.py
+++ Example_02_05.py
@@ -10,27 +10,26 @@ K = Kc * (1 + 1 / (Tauc * s))
 L = K * G
 T = feedback(L, 1)
 S = feedback(1, L)
-
+u = S * K
 
 plt.figure('Figure 2.9')
 tspan = np.linspace(0, 4, 50)
 [t, y] = T.step(0, tspan)
 plt.plot(t, y)
-[t, y] = S.step(0, tspan)
-plt.plot(t, y)
+[t, u] = u.step(0, tspan)
+plt.plot(t, u, '-.')
 plt.plot([0, 4], np.ones(2))
 plt.xlabel('Time [s]')
 plt.legend(['y(t)','u(t)'])
-#TODO there is a descrepancy with the u(t) plot
 
 bodeclosedloop(G, K, -1, 2, label='Figure 2.15', margin=True)
-#TODO there is a descrepancy with the phase plots
+#TODO there is a discrepancy with the phase plots
 
 GM, PM, wc, wb, wbt, valid = marginsclosedloop(L) 
-print 'GM :' , GM
-print "PM:", np.round(PM*180/np.pi, 1), "deg or", np.round(PM, 2), "rad"
-print 'wb :' , wb
-print 'wc :' , wc
-print 'wbt :' , wbt
+print 'GM:' , np.round(GM, 2)
+print 'PM:', np.round(PM / 180 * np.pi, 2), "rad or", np.round(PM, 2), "deg"
+print 'wb:' , np.round(wb, 2)
+print 'wc:' , np.round(wc, 2)
+print 'wbt:' , np.round(wbt, 4)
 
 plt.show()
