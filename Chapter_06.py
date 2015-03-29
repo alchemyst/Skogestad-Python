37a8faf756e473853028497aef92d38457142957
Carl Sandrock <carl.sandrock@up.ac.za>
2015-03-29 09:55:05 +0200

    Raise exception rather than call sys.exit()

--- Chapter_06.py
+++ Chapter_06.py
@@ -118,8 +118,7 @@ def Min_Peaks(Zeros_G, Poles_G, error_poles_direction, io_type, usedeadtime=Fals
             p_direction[:, i] = S[:, 0]   
             
     else:
-        print "Incorrect io_type specified"
-        sys.exit
+        raise ValueError("Incorrect io_type specified")
 
     yz_mat1 = (np.matrix(np.diag(Zeros_G)) *
                np.matrix(np.ones([len(Zeros_G), len(Zeros_G)])))
