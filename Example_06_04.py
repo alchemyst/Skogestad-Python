85a8d9fca5f51585513f02b048b7378aea585495
Schalk van Heerden <vhschalk@gmail.com>
2015-04-24 17:39:18 +0200

    mimotf issue example

--- Example_06_04.py
+++ Example_06_04.py
@@ -1,13 +1,13 @@
 from utils import pole_zero_directions, BoundKS, tf, mimotf
 from reporting import display_export_data
-import numpy as np
+
 s = tf([1, 0])
 
 G11 = (s - 2.5) / (s - 2)
 G12 = -(0.1 * s + 1) / (s + 2)
 G21 = (s - 2.5) / (0.1 * s + 1)
-# TODO the mimotf does not accept constants in a cell
-G22 = s # should be 1!
+# TODO ISSUE the mimotf does not accept constants in a cell
+G22 = 1
 
 G = mimotf([[G11, G12],
             [G21, G22]])
@@ -17,13 +17,13 @@ z = G.zeros()
 print 'Poles: {0}'.format(p)
 print 'Zeros: {0}'.format(z)
 print ''
-
-def Gs(s):
-    G11 = (s + 2.5) / (s + 2)
-    G12 = -(0.1 * s + 1) / (s + 2)
-    G21 = (s + 2.5) / (0.1 * s + 1)   
-    G22 = 1
-    return np.matrix([[G11, G12],[G21, G22]])
+    
+G11 = (s + 2.5) / (s + 2)
+G12 = -(0.1 * s + 1) / (s + 2)
+G21 = (s + 2.5) / (0.1 * s + 1)   
+G22 = 1    
+Gs = mimotf([[G11, G12],
+             [G21, G22]])
     
 # select RHP-pole
 p = [2.]
