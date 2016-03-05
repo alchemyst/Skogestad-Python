85c489c19653360bc214cdeab065f711ee5c2e7d
marcelledekock <marcelledk@mintek.co.za>
2016-03-05 18:11:35 +0200

    include future and change print

--- Example_02_09.py
+++ Example_02_09.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import __future__
 from utils import tf
 
 
@@ -24,7 +24,7 @@ plt.xlabel('Frequency (rad/s)')
 
 # numeric solution to find wc
 wc = w[np.flatnonzero(np.abs(Gdfr) < 1)[0]]
-print 'wc:', wc
+print ('wc:', wc)
 # the solution of wc: 10 is an estimate in the handbook
 
 s = tf([1, 0], 1)
