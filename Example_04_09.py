564d4348faaac913921b87708c375cd6b0b464c5
Neill Herbst <n.herbst6@gmail.com>
2016-03-28 16:51:47 +0200

    addition of 'print_function'

--- Example_04_09.py
+++ Example_04_09.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import sympy as sp
 
 from utils import poles, zeros
@@ -5,6 +6,6 @@ from utils import poles, zeros
 def G(s):
     return 1 / (1.25 * (s + 1) * (s + 2)) * sp.Matrix([[s - 1, s],
                                                        [-6, s - 2]])
-                                                
-print 'Poles: ' , poles(G)
-print 'Zeros: ' , zeros(G)
+
+print('Poles: ' , poles(G))
+print('Zeros: ' , zeros(G))
