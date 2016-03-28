fcb85c5870fea4e5e890ae54c59d14b650f90c1a
Neill Herbst <n.herbst6@gmail.com>
2016-03-28 16:52:06 +0200

    PEP8

--- Example_04_09.py
+++ Example_04_09.py
@@ -7,5 +7,5 @@ def G(s):
     return 1 / (1.25 * (s + 1) * (s + 2)) * sp.Matrix([[s - 1, s],
                                                        [-6, s - 2]])
 
-print('Poles: ' , poles(G))
-print('Zeros: ' , zeros(G))
+print('Poles: ', poles(G))
+print('Zeros: ', zeros(G))
