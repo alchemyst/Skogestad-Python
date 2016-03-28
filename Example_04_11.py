4815a8733c756f58fdfe08f6065125a5049d7bfd
Neill Herbst <n.herbst6@gmail.com>
2016-03-28 16:55:14 +0200

    Addition of print function

--- Example_04_11.py
+++ Example_04_11.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import numpy as np
 
 from utils import poles, zeros
@@ -5,5 +6,5 @@ from utils import poles, zeros
 def G(s):
     return 1 / (s + 2) * np.matrix([[s - 1,  4],
                                     [4.5, 2 * (s - 1)]])
-print 'Poles: ' , poles(G)
-print 'Zeros: ' , zeros(G)
+print('Poles: ' , poles(G))
+print('Zeros: ' , zeros(G))
