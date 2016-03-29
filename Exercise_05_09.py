3c73a1ecc89fa48c0641a87f8ebb075f49efb8b9
MrMikeWolf <u12107949@tuks.co.za>
2016-03-29 12:02:07 +0200

    PEP8 formatting and gardening

--- Exercise_05_09.py
+++ Exercise_05_09.py
@@ -9,24 +9,24 @@ import matplotlib.pyplot as plt
 
 
 def omega_p(s, k, M):
-    return ((1000*s/k+1/M)*(s/(M*k)+1))/((10*s/k+1)*(100*s/k+1))
+    return ((1000 * s / k + 1 / M) * (s / (M * k) + 1)) / ((10 * s / k + 1) * (100 * s / k + 1))
 
-#plotting M = 2 and w_B = 1
-ws = np.logspace(-4, 4, 1000)
+
+# plotting M = 2 and w_B = 1
+
+ws = np.logspace(-4, 4, 1000)
 ks = [0.1, 0.5, 1.0, 10.0, 100.0, 1000.0, 2000.0, 10000.0]
 
 omegas = []
 
 for w in ws:
-    s = 1j*w
-    omegas.append(1/np.abs(omega_p(s, 1.0, 2.0)))
+    s = 1j * w
+    omegas.append(1 / np.abs(omega_p(s, 1.0, 2.0)))
 
 plt.loglog(ws, omegas)
 plt.xlabel("Freq")
 plt.ylabel("1/W_p")
 plt.show()
 
-
 for k in ks:
-    print "For k = ", k, ", Wp(z) = ", omega_p(1.0, k, 2.0)
- 
+    print("For k = {} Wp(z) = {} ".format(k, omega_p(1.0, k, 2.0)))
