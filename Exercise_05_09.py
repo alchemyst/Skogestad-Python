cda77b1be4ce17da19890ab595c345bb33491891
Edgar <edgar.whyte@gmail.com>
2016-03-29 09:05:40 +0200

    make printing Python  2 and Python 3 compatible

--- Exercise_05_09.py
+++ Exercise_05_09.py
@@ -4,15 +4,18 @@ Created on Jun 6, 2013
 @author: FENNERK
 '''
 
+from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def omega_p(s, k, M):
-    return ((1000*s/k+1/M)*(s/(M*k)+1))/((10*s/k+1)*(100*s/k+1))
+    return ((1000*s/k+1/M)*(s/(M*k)+1))/((10*s/k+1)*(100*s/k+1))
+
 
 #plotting M = 2 and w_B = 1
-ws = np.logspace(-4, 4, 1000)
+
+ws = np.logspace(-4, 4, 1000)
 ks = [0.1, 0.5, 1.0, 10.0, 100.0, 1000.0, 2000.0, 10000.0]
 
 omegas = []
@@ -28,5 +31,5 @@ plt.show()
 
 
 for k in ks:
-    print "For k = ", k, ", Wp(z) = ", omega_p(1.0, k, 2.0)
- 
+    print("For k = ", k, ", Wp(z) = ", omega_p(1.0, k, 2.0))
+
