033dd33068226f6f42fed2db77e2b9ffbed72943
MrMikeWolf <u12107949@tuks.co.za>
2016-03-29 15:08:02 +0200

    PEP8 formatting

--- Example_02_07.py
+++ Example_02_07.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal as scs
 
+
 # Example 2.7 compares w_B and w_BT as indicators of performance
 # it illustrates that w_B might be a better indicator, but care
 # must be taken and the phase must also be considered.
@@ -10,8 +11,9 @@ import scipy.signal as scs
 # Creates a general transfer function based on the coefficients
 # of the numerator and denominator
 def TF(num, den, s):
-    TF = np.polyval(num, s)/np.polyval(den, s)
+    TF = np.polyval(num, s) / np.polyval(den, s)
     return TF
+
 
 # Initialize freq range and other constants
 w = np.logspace(-2, 2, 100)
@@ -20,7 +22,7 @@ tau = 1
 
 # Coeff found in sage of the T function
 Tnum = [-1, z]
-Tden = [tau, tau*z + 1, z]
+Tden = [tau, tau * z + 1, z]
 
 # Time domain output of a step response
 [Tim, yout, xout] = scs.lsim((Tnum, Tden), np.ones(np.size(w)), w)
@@ -31,18 +33,18 @@ plt.ylabel('y(t)')
 plt.xlabel('Time [s]')
 
 # Gain of both S and T transfer functions
-Tgain = np.abs([TF(Tnum, Tden, (1j*i)) for i in w])
+Tgain = np.abs([TF(Tnum, Tden, (1j * i)) for i in w])
 
-Sgain = np.abs([TF(Tnum, Tden, (1j*i))-1 for i in w])
+Sgain = np.abs([TF(Tnum, Tden, (1j * i)) - 1 for i in w])
 
 # Plot Gains
 plt.subplot(2, 1, 2)
 plt.loglog(w, Tgain)
 plt.loglog(w, Sgain)
-#TODO plot w180, wbt
-plt.legend(["|T|","|S|"])   
+# TODO plot w180, wbt
+plt.legend(["|T|", "|S|"])
 plt.ylabel('Magnitude')
 plt.xlabel('Frequency [rad/s]')
 plt.show()
 
-#TODO GM, PM, Ms, Mt, wb, wc
+# TODO GM, PM, Ms, Mt, wb, wc
