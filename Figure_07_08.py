efdbe15dae5ecab615aa7d56e64cfcc1678bfef9
MrMikeWolf <u12107949@tuks.co.za>
2016-04-16 17:49:24 +0200

    Created function to setup bode plot in Figure_07_08

--- Figure_07_08.py
+++ Figure_07_08.py
@@ -8,6 +8,8 @@ Created on Mon Jun 03 23:16:52 2013
 #Figure 7.8
 import numpy as np
 import matplotlib.pyplot as plt
+import doc_func as df
+
 # Time delay
 w = np.logspace(-2, 2, 1000)
 s = 1j * w
@@ -16,23 +18,23 @@ l_delay = 1 - np.exp(-thetap * s)
 tau = 1
 l_nolag = 1 - (1/(1 + tau * s))
 
-plt.title('Time Delay Value over Frequency')
-plt.figure(1)
-plt.xlabel(r'Frequency [rad/s]', fontsize=14)
-plt.ylabel(r'Magnitude', fontsize=15)
-plt.loglog(w, np.abs(l_delay))
-plt.loglog(w, (np.ones(len(w))), '-.')
-plt.vlines(1, 10**(-3), 1, color='m', linestyle='dashed')
-plt.ylim(-2, 10)
-plt.grid(b=None, which='both', axis='both')
-plt.legend(('time delay', '1', 'theta_max'), loc=3)
-plt.figure(2)
-plt.xlabel(r'Frequency [rad/s]', fontsize=14)
-plt.ylabel(r'Magnitude', fontsize=15)
-plt.loglog(w, np.abs(l_nolag))
-plt.loglog(w, (np.ones(len(w))), '-.')
-plt.vlines(1, 10**(-2), 1, color='m', linestyle='dashed')
-plt.ylim(-2, 10)
-plt.grid(b=None, which='both', axis='both')
-plt.legend(('First-order Lag', '1', '1/tau_max'), loc=1)
+
+def setup_bode(G, p, func_string, max_param,  w=None):
+    plt.figure()
+    plt.xlabel(r'Frequency [rad/s]', fontsize=14)
+    plt.ylabel(r'Magnitude', fontsize=15)
+    plt.loglog(w, abs(G))
+    plt.loglog(w, (ones(len(w))), '-.')
+    plt.vlines(1, 10**(p), 1, color='m', linestyle='dashed')
+    plt.ylim(-2, 10)
+    plt.grid(b=None, which='both', axis='both')
+    plt.legend((func_string, '1', max_param), loc=3)
+
+func = [[l_delay, -2, 'time delay', r'$\theta_{max}$'],
+        [l_nolag, -3, 'First order lag', r'$1/\tau_{max}$']]
+
+for function, power, func_string, max_param in func:
+    df.setup_bode(function, power, func_string, max_param, w)
+
 plt.show()
+
