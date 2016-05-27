9e8b57e850be9ec58de899f97e260dee45e345b3
marcelledekock <marcelledk@mintek.co.za>
2016-05-27 06:56:42 +0200

    made compatible with new pole, zero calc

--- Example_04_10.py
+++ Example_04_10.py
@@ -2,18 +2,18 @@ from __future__ import print_function
 import sympy
 import utils
 
-def G(s):
-    M = sympy.Matrix([[(s - 1)*(s + 2), 0, (s - 1)**2],
+s = sympy.Symbol('s')
+M = sympy.Matrix([[(s - 1)*(s + 2), 0, (s - 1)**2],
                       [-(s + 1)*(s + 2), (s - 1)*(s + 1),  (s - 1)*(s + 1)]])
-    return (1/((s + 1)*(s + 2)*(s - 1)))*M
+G = (1/((s + 1)*(s + 2)*(s - 1)))*M
 
 poles = utils.poles(G)
 
 print("The poles of the system are = ",poles)
 
 # TODO: This should work correctly
-# s = utils.tf([1, 0], 1)
-# M = utils.mimotf([[(s - 1) * (s + 2), 0, (s - 1) ** 2],
-#                   [-(s + 1) * (s + 2), (s - 1) * (s + 1), (s - 1) * (s + 1)]])
-# G = 1/((s + 1)*(s + 2)*(s - 1)) * M
-# print("The poles of the system are = ", G.poles())
+s = utils.tf([1, 0], 1)
+M = utils.mimotf([[(s - 1) * (s + 2), 0, (s - 1) ** 2],
+                   [-(s + 1) * (s + 2), (s - 1) * (s + 1), (s - 1) * (s + 1)]])
+G = 1/((s + 1)*(s + 2)*(s - 1)) * M
+print("The poles of the system are = ", G.poles())
