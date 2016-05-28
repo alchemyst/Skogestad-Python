4a57abb1e6dc12fe458b689f821b3b8871b33e02
marcelledekock <marcelledk@mintek.co.za>
2016-05-28 14:24:20 +0200

    correction on weight function & improved plot colours

--- Figure_07_09.py
+++ Figure_07_09.py
@@ -1,13 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from math import pi
 from utils import tf
-
 
 def LI(rk, tmax, w):
     L = np.zeros(np.size(w))
     for i in range(np.size(w)):
-        if w[i-1] < 3.14159/tmax:
+        if w[i-1] < pi/tmax:
             L[i-1] = np.sqrt(rk**2 + 2*(1+rk)*(1-np.cos(tmax*w[i-1])))
         else:
             L[i-1] = 2 + rk
@@ -16,17 +15,17 @@ def LI(rk, tmax, w):
 rk = 0.2
 tmax = 1
 
-WI = tf([1 + rk*0.5, rk], [tmax*0.5, 1])
+WI = tf([(1 + rk*0.5)*tmax, rk], [tmax*0.5, 1])
 WI_improved = WI*tf([(tmax/2.363)**2, 2*0.838*(tmax/2.363), 1], [(tmax/2.363)**2, 2*0.685*(tmax/2.363), 1])
 
 w = np.logspace(-2, 2, 500)
 s = 1j*w
 
-plt.loglog(w, LI(rk, tmax, w), 'b', label='$l_I$')
-plt.loglog(w, np.abs(WI(s)), 'k--', label='$\omega_I$')
-plt.loglog(w, np.abs(WI_improved(s)), 'r*', label='$improved$ $\omega_I$')
+plt.loglog(w, LI(rk, tmax, w), 'r', label='$l_I$')
+plt.loglog(w, np.abs(WI(s)), 'b--', label='$\omega_I$')
+plt.loglog(w, np.abs(WI_improved(s)), color = 'lime', label='$improved$ $\omega_I$')
 plt.legend()
 plt.title(r'Figure 7.9')
-plt.xlabel(r'Frequency [rad/s]', fontsize=14)
-plt.ylabel(r'Magnitude', fontsize=15)
+plt.xlabel(r'Frequency [rad/s]', fontsize=12)
+plt.ylabel(r'Magnitude', fontsize=12)
 plt.show()
