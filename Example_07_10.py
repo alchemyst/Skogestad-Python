58c11a7a4a33b578da8874217b3e7b607073cd64
marcelledekock <marcelledk@mintek.co.za>
2016-05-30 16:17:47 +0200

    use symbolic solver instead of increment

--- Example_07_10.py
+++ Example_07_10.py
@@ -1,6 +1,7 @@
 import utils
 import numpy as np
 import sympy as sp
+from scipy.optimize import fsolve
 from cmath import sqrt
 
 s = utils.tf([1,0],[1])
@@ -13,7 +14,6 @@ print('GM = 1/|L(w180)|',GM)
 print('From 7.55, kmax = GM = ',GM)
 
 omega = np.logspace(-2,2,1000)
-s = 1j*omega
 
 def rk(kmax):
     return (kmax - 1)/(kmax + 1)
@@ -22,16 +22,9 @@ def kbar(kmax):
     return (kmax + 1)/2
 
 def RScondition(kmax):
-    ineq = [abs(rk(kmax) * kbar(kmax) * L(i)/(1 + kbar(kmax)*L(i))) for i in s]
-    return max(ineq)
+    abs_ineq = [abs(rk(kmax) * kbar(kmax) * L(1j*s)/(1 + kbar(kmax)*L(1j*s))) for s in omega]
+    max_ineq = max(abs_ineq) - 1
+    return max_ineq
 
-# note: this itterative solution method was used, 
-#because sympy's solve function does not accept equations with complex numbers
-
-kmax = 1
-ineq = 0
-while ineq < 1:
-    ineq = RScondition(kmax)
-    kmax += 0.001
-
-print('From 7.58, kmax = ',np.round(kmax)) 
+kcal = fsolve(RScondition,1)
+print('From 7.58, kmax = ',np.round(kcal,2))
