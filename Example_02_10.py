15387acdce16cf2c4e78334eab05488f5fdf4f27
marcelledekock <marcelledk@mintek.co.za>
2016-06-02 19:13:43 +0200

    PEP8 formatting

--- Example_02_10.py
+++ Example_02_10.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 
 from utils import tf
 
-
-s = tf([1, 0], 1)
+s = tf([1,0], 1)
 
 # Set-up plant and Disturbance model
 G = 200/((10*s + 1)*(0.05*s + 1)*(0.05*s + 1))
@@ -20,8 +19,7 @@ G_d = 100/(10*s + 1)
 Ks = [((10*(10*s + 1))*(0.1*s + 1))/(s*200*(0.01*s + 1)),
       0.5,
       0.5*(s + 2)/s,
-      (0.5*(s + 2)*(0.05*s + 1))/(s*(0.005*s + 1)),
-      ]
+      (0.5*(s + 2)*(0.05*s + 1))/(s*(0.005*s + 1))]
 
 w = np.logspace(-2, 2, 100)
 s = 1j*w
@@ -30,7 +28,7 @@ tspan = np.linspace(0, 3, 100)
 for K in Ks:
     # Closed loop tf"""
     L = G*K
-    cont = (1/(1+L))*G_d
+    cont = (1/(1 + L))*G_d
     # Step response in the disturbance"""
     [t, y] = cont.step(0, tspan)
 
