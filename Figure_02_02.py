0fcb5f7c5a9a2b72c5a35980c54c8e509d23aa9f
92215302 <jonathan.vanniekerk@icloud.com>
2017-02-06 21:57:16 +0200

    Fig 2.2. Delay implemented using Pade approximation
    Alternative approach using Pade approximation from the python-control
    systems library.

--- Figure_02_02.py
+++ Figure_02_02.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-from utils import tf
-from utilsplot import bode
+from control import bode_plot, tf, pade
+from numpy import linspace
 
-G = tf([5], [10, 1], deadtime=2)
-
+G = tf([5], [10, 1])        #Define the transfer function
+num, den = pade(2, n=10)     #Generate a 10th order pade approximation for 2s delay
+P = tf(num, den)            #General transfer function of the delay
+Gdelay = G*P                #Apply the delay to the transfer function
 plt.figure('Figure 2.2')
-plt.title('Bode plots of G(s)')
-bode(G, -3, 1)
-plt.show()
+bode_plot(Gdelay, omega=linspace(0.001, 10, 1000))
