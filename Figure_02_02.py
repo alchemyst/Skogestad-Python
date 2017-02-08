1b56a9a5479c099ed7d0c6a2e244fa3ddba36583
92215302 <jonathan.vanniekerk@icloud.com>
2017-02-08 20:46:49 +0200

    Revert "Fig 2.2. Delay implemented using Pade approximation"
    This reverts commit 0fcb5f7c5a9a2b72c5a35980c54c8e509d23aa9f.

--- Figure_02_02.py
+++ Figure_02_02.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-from control import bode_plot, tf, pade
-from numpy import linspace
+from utils import tf
+from utilsplot import bode
 
-G = tf([5], [10, 1])        #Define the transfer function
-num, den = pade(2, n=10)     #Generate a 10th order pade approximation for 2s delay
-P = tf(num, den)            #General transfer function of the delay
-Gdelay = G*P                #Apply the delay to the transfer function
+G = tf([5], [10, 1], deadtime=2)
+
 plt.figure('Figure 2.2')
-bode_plot(Gdelay, omega=linspace(0.001, 10, 1000))
+plt.title('Bode plots of G(s)')
+bode(G, -3, 1)
+plt.show()
