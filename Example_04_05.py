2f861a3f5c888b5d36eda840dd7f393d549f1599
92215302 <jonathan.vanniekerk@icloud.com>
2017-04-16 11:12:03 +0200

    Pull comments addressed

--- Example_04_05.py
+++ Example_04_05.py
@@ -1,8 +1,6 @@
 import sympy
 import numpy as np
 
-sympy.init_printing(use_latex='Mathjax')
-
 t = sympy.Symbol('t')
 
 A = sympy.Matrix([[-1/t, 0, 0, 0],
@@ -34,13 +32,13 @@ control_matrix = np.hstack(c_plus)
 # Sympy matrix
 C_matrix = sympy.Matrix(np.zeros_like(A))
 for r in range(0, A.shape[1]):
-        for c in range(A.shape[1]):
-            C_matrix[r,c] = control_matrix[r,c]
+    for c in range(A.shape[1]):
+        C_matrix[r,c] = control_matrix[r,c]
             
 print('\nRank of controllability matix =', C_matrix.rank())                      
 if C_matrix.rank() == C_matrix.shape[1] :
-    print('Controllability', C_matrix, \
+    print('Controllability', C_matrix,
           '\nhas full rank and is state controllable')
 else:
-    print('Controllability', C_matrix, \
+    print('Controllability', C_matrix,
           '\ndoes not have full rank and is not state controllable')
