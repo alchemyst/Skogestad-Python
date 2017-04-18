751ac25b08091732445778d53978e5f11b928d39
Wiid <Andries.Wiid@sasol.com>
2017-04-18 18:20:24 +0200

    PEP8

--- Figure_02_07.py
+++ Figure_02_07.py
@@ -5,7 +5,7 @@ from utilsplot import bode
 
 # Trasfer function of L(s)
 s = tf([1, 0], 1)
-L =3*(-2*s + 1)/((10*s + 1)*(5*s + 1))
+L = 3*(-2*s + 1)/((10*s + 1)*(5*s + 1))
 
 plt.figure('Figure 2.7')
 plt.title('Bode plots of L(s) with Kc = 1')
