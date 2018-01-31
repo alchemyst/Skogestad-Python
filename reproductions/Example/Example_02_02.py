a76b615074aa9e107e4307b22364a8a704b37d45
Shaun <shaun@bluenickel.co.za>
2018-01-31 08:26:55 +0200

    Added comments to example file

--- reproductions/Example/Example_02_02.py
+++ reproductions/Example/Example_02_02.py
@@ -7,11 +7,12 @@ from utils import feedback, tf
 s = tf([1, 0], 1)
 G = 3*(-2*s + 1)/((10*s + 1)*(5*s + 1))
 
+# Configure plot and generate timespan
 tspan = np.linspace(0, 50, 100)
 plt.figure('Figure 2.6')
 plt.title('Effect of proportional gain Kc on closed loop response')
 
-#  calculate the time domain response
+#  Calculate the time domain response
 Ks = [0.5, 1.5, 2, 2.5, 3.0]
 for K in Ks:
     T = feedback(G * K, 1)
@@ -21,6 +22,7 @@ for K in Ks:
     else:
         plt.plot(t, y)
 
+# Plot time domain response
 plt.legend(["Kc = %1.1f" % K for K in Ks])
 plt.xlabel('Time [s]')
 plt.ylim(-0.5, 2.5)
