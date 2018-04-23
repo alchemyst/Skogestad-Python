a42b7147715ec1273b4e9cefb8a45127a7b2341d
Deon Pretorius <u13160312@tuks.co.za>
2018-04-23 19:22:56 +0200

    Validity test for pole_zero_directions (#311)
    Test whether valid pole or zero directions were determined. Method basically tests whether substitution of pole or zero in process matrix multiplied by direction gives small or large value.

--- utils.py
+++ utils.py
@@ -2290,7 +2290,7 @@ def zeros(G=None, A=None, B=None, C=None, D=None):
                 else:
                     gcd = polygcd(gcd, numpy.poly1d(num_coeff))
             else:
-                gcd = poly1d(numer)
+                gcd = numpy.poly1d(numer)
         zero = list(set(numpy.roots(gcd)))
         pole = poles(G)
         for i in pole:
@@ -2312,7 +2312,7 @@ def zeros(G=None, A=None, B=None, C=None, D=None):
         # return sympy.solve((M - z*Ig).det(), z)
 
 
-def pole_zero_directions(G, vec, dir_type, display_type='a', e=0.00001):
+def pole_zero_directions(G, vec, dir_type, display_type='a', e=1E-8, min_tol=1E-5, max_tol=1E4):
     """
     Crude method to calculate the input and output direction of a pole or zero,
     from the SVD.
@@ -2347,6 +2347,12 @@ def pole_zero_directions(G, vec, dir_type, display_type='a', e=0.00001):
 
     e : float
         Avoid division by zero. Let epsilon be very small (optional).
+
+    min_tol : float
+        Acceptable tolerance for zero validation. Let min_tol be very small (optional).
+
+    max_tol : float
+        Acceptable tolerance for pole validation. Let max_tol be very small (optional).
 
     Returns
     -------
@@ -2390,8 +2396,20 @@ def pole_zero_directions(G, vec, dir_type, display_type='a', e=0.00001):
         u = V[:, dt]
         y = U[:, dt]
 
-# TODO complete validation test
-        v = True
+        # validation
+        v = False
+
+        # zero validation test
+        if dir_type == 'z':
+            z_test = min(abs(g*u).A1)
+            if z_test <= min_tol:
+                v = True
+
+        # pole validation test
+        if dir_type == 'p':
+            p_test = max(abs(g*u).A1)
+            if p_test >= max_tol:
+                v = True
 
         if display_type == 'u':
             pz_dir[:, i] = u
