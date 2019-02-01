3c3df99dc2eb1f9c617324c913cc9d1a449971b5
WillemASmit <wianwasmit@gmail.com>
2019-02-01 10:33:34 +0200

    Updated Exercise 03_05

--- reproductions/Exercise/Exercise_03_05.py
+++ reproductions/Exercise/Exercise_03_05.py
@@ -3,15 +3,15 @@ import numpy as np
 
 
 # Define functions
-G_3_29 = np.matrix([[5, 4], [3, 2]])
-G_3_30 = np.matrix([[0, 100], [0, 0]])
+G_3_29 = np.array([[5, 4], [3, 2]])
+G_3_30 = np.array([[0, 100], [0, 0]])
 
 def norms(G, Matrix_input):
     """Function to calculate norms"""
     spectrad = max(np.linalg.eigvals(G))
     Frob_norm = np.linalg.norm(G, 'fro')
     B = np.cumsum(abs(G))
-    Sum_norm = B[0, G.size-1]
+    Sum_norm = B[G.size-1]
     Columnsum = np.max(np.sum(abs(G),axis=0))
     Rowsum = np.max(np.sum(abs(G),axis=1))
     [U, S, V] = np.linalg.svd(G)
@@ -28,6 +28,7 @@ def norms(G, Matrix_input):
 norms(G_3_29, "Matrix 3.29")
 print('')
 norms(G_3_30, "Matrix 3.30")
+
 
 
 
