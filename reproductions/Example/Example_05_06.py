2f134b0ff69a0fe4d931540c32db1935d70fd428
Bern <bern@analyte.co.za>
2020-03-29 18:23:26 +0200

    Tested all notebooks with test_all_notebooks.py, updated .testignore

--- reproductions/Example/Example_05_06.py
+++
@@ -1,2 +0,0 @@
-# calls Figure 5.7 to be shown for the solution to Example 5.6
-execfile('Figure_05_07.py')
--- reproductions/Example/Example_05_07.py
+++
@@ -1,3 +0,0 @@
-#calls Figure 5.7 & 5.8 to be shown for the solution to Example 5.7
-execfile('Figure_05_07.py')
-execfile('Figure_05_08.py')
