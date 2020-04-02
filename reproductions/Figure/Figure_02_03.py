22d2f10e56a4f5d82750a376148389052d25fca4
Sikhu-Ntaka <60363208+Sikhu-Ntaka@users.noreply.github.com>
2020-04-02 02:33:48 +0200

    Title added to Bode Plot
    Added a title to the bode plot as per text book

--- reproductions/Figure/Figure_02_03.py
+++ reproductions/Figure/Figure_02_03.py
@@ -1,3 +1,4 @@
+
 import matplotlib.pyplot as plt
 
 from utils import tf
@@ -8,4 +9,6 @@ G = 30*(s + 1)/((s + 10)*(s + 0.01)**2)
 
 plt.figure('Figure 2.3')
 bode(G, -3, 3)
+plt.title('Figure 2.3: Bode plot of transfer function ' + r'$L_1 = \frac{30(s + 1)}{(s + 10)(s + 0.01)^2}$' 
+          + '\n', fontsize = 13, y = -1)
 plt.show()
