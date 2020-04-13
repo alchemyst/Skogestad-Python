19b3fdd1504d3be6c1dd9554283208a6eade6f49
TrisSmanjak <TrisSmanjak@users.noreply.github.com>
2020-04-13 16:47:45 +0200

    Revert "Revert "Merge remote-tracking branch 'upstream/master'""
    This reverts commit f0b5564513b79a007f3f1aee5247430b60c51041.

--- reproductions/Example/Example_04_11.py
+++
@@ -1,7 +0,0 @@
-from __future__ import print_function
-from utils import poles, zeros, tf, mimotf
-
-s = tf([1, 0], [1])
-G =  1 / (s + 2) * mimotf([[s - 1, 4], [4.5, 2 * (s - 1)]])
-print('Poles: ', poles(G))
-print('Zeros: ', zeros(G))
